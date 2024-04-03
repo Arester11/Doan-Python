15ee424d2360ef0e34799479afc40a7594b4f0b0
Arester <chiphongnguyen2610@gmail.com>
2024-04-03 13:43:13 +0700

    doi jump thanh nut space, dash thanh nut Lshift

--- gameplay.py
+++ gameplay.py
@@ -99,20 +99,20 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type ==pygame.KEYDOWN:
-                    if event.key==pygame.K_a:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
                         self.movement[0] = True
-                    if event.key==pygame.K_d:
-                        self.movement[1]=True
-                    if event.key==pygame.K_w:
-                        self.player.jump()
-                    if event.key==pygame.K_SPACE:
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True
+                    if event.key == pygame.K_LSHIFT:
                         self.player.dash()
-                if event.type ==pygame.KEYUP:
-                    if event.key==pygame.K_a:
+                    if event.key == pygame.K_SPACE:
+                        self.player.jump()
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
                         self.movement[0] = False
-                    if event.key==pygame.K_d:
-                        self.movement[1]=False
+                    if event.key == pygame.K_d:
+                        self.movement[1] = False
             
             self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0))
             pygame.display.update()
